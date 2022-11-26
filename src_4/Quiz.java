7ced5e6f8e18304786ede9c7b1fa1ef8285a9458
Eungyo Song <songeg09@gmail.com>
2022-11-26 04:37:44 -0700

    More progress

---
+++ src_4/Quiz.java
@@ -0,0 +1,78 @@
+package src_4;
+
+public class Quiz {
+
+    private String question;
+    private String choice1;
+    private String choice2;
+    private String choice3;
+    private String choice4;
+    private int answer;
+    private int point;
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getChoice1() {
+        return choice1;
+    }
+
+    public void setChoice1(String choice1) {
+        this.choice1 = choice1;
+    }
+
+    public String getChoice2() {
+        return choice2;
+    }
+
+    public void setChoice2(String choice2) {
+        this.choice2 = choice2;
+    }
+
+    public String getChoice3() {
+        return choice3;
+    }
+
+    public void setChoice3(String choice3) {
+        this.choice3 = choice3;
+    }
+
+    public String getChoice4() {
+        return choice4;
+    }
+
+    public void setChoice4(String choice4) {
+        this.choice4 = choice4;
+    }
+
+    public int getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
+    public int getPoint() {
+        return point;
+    }
+
+    public void setPoint(int point) {
+        this.point = point;
+    }
+
+    public Quiz(String question, String choice1, String choice2, String choice3, String choice4, int answer, int point) {
+        this.question = question;
+        this.choice1 = choice1;
+        this.choice2 = choice2;
+        this.choice3 = choice3;
+        this.choice4 = choice4;
+        this.answer = answer;
+        this.point = point;
+    }
+}
